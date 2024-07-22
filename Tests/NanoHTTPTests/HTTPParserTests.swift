26e36bcb571b455615dc0e0945f1afe009eab423
Matthias Zenger <matthias@objecthub.net>
2024-07-22 15:39:10 +0300

    Include tests.

---
+++ Tests/NanoHTTPTests/HTTPParserTests.swift
@@ -0,0 +1,8 @@
+//
+//  HTTPParserTests.swift
+//  NanoHTTPTests
+//
+//  Created by Matthias Zenger on 22/07/2024.
+//
+
+import Foundation
---
+++ Tests/NanoHTTPTests/MimeTypeTests.swift
@@ -0,0 +1,8 @@
+//
+//  MimeTypeTests.swift
+//  NanoHTTPTests
+//
+//  Created by Matthias Zenger on 22/07/2024.
+//
+
+import Foundation
